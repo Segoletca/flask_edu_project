1569dd166ffd741f642b7c0549abea0d5158a13d
Segoletca <sergey.noskov.2001@mail.ru>
2024-09-02 21:28:21 +0300

    Create products create on list

--- rest/products/views.py
+++ rest/products/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, url_for, redirect
 
 from .crud import products_storage
 
@@ -17,3 +17,23 @@ def get_products_list():
         "products/list.html",
         products=products,
     )
+
+
+@app.post("/", endpoint="create")
+def create_product():
+    product_name = request.form.get("product-name", "").strip()
+    product_price = request.form.get("product-price", "").strip()
+
+    # if product_price.isdigit()
+    product = products_storage.add(
+        product_name=product_name,
+        product_price=int(product_price),
+    )
+    # products = products_storage.get_list()
+    #
+    # return render_template(
+    #     "products/list.html",
+    #     products=products,
+    # )
+    url = url_for("products_app.list")
+    return redirect(url)
