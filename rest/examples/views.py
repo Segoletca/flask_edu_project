ec89bb62131adcfa8a41dc675243732dbe23e52d
Segoletca <sergey.noskov.2001@mail.ru>
2024-10-06 00:42:16 +0300

    Add CSRF tokens or exempt to requests

--- rest/examples/views.py
+++ rest/examples/views.py
@@ -1,6 +1,11 @@
 from flask import Blueprint, render_template, request
 
-examples_app = Blueprint("examples_app", __name__, )
+from csrf_protection import csrf
+
+examples_app = Blueprint(
+    "examples_app",
+    __name__,
+)
 
 app = examples_app
 
@@ -10,17 +15,23 @@ def examples_list():
     return render_template("examples/index.html")
 
 
-@app.route("/ping/",
-           methods=["GET", "POST"],
-           endpoint="ping",
-           )
+@app.route(
+    "/ping/",
+    methods=["GET", "POST"],
+    endpoint="ping",
+)
 def handle_ping():
     if request.method == "POST":
         return "Pong!"
     return render_template("examples/ping/show_ping.html")
 
 
-@app.route("/hover/", methods=["GET", "POST"], endpoint="hover", )
+@app.route(
+    "/hover/",
+    methods=["GET", "POST"],
+    endpoint="hover",
+)
+@csrf.exempt
 def handle_hover():
     if request.method == "POST":
         return "I see you!"
