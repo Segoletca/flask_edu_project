0eb1c24315d03bb5e15a4ea0589e0d64dd23ff28
Segoletca <sergey.noskov.2001@mail.ru>
2024-10-06 00:17:03 +0300

    Add CSRF protection

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 
+from csrf_protection import csrf
 from rest.index import index_app
 from rest.examples import examples_app
 from rest.clicker import clicker_app
@@ -8,6 +9,11 @@ from rest.products import products_app
 
 def create_app():
     app = Flask(__name__)
+    app.config.update(
+        # python -c 'import secrets; print(secrets.token_hex())'
+        SECRET_KEY='ebebb939c6beaa9ea23c1fb581a35b80318dd0328647b73b091a66dd652aa335',
+    )
+    csrf.init_app(app)
     app.register_blueprint(index_app)
     app.register_blueprint(examples_app, url_prefix="/examples", )
     app.register_blueprint(clicker_app, url_prefix="/clicker", )
